138f0675bd59805dc724b57443191e4d42531877
leo.pellan <leo.pellan@epita.fr>
2022-09-21 22:01:21 +0200

    oui

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
+
+import random
 from discord.ext import commands
 import discord
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
+intents.presences = True
 bot = commands.Bot(
     command_prefix="!",  # Change to desired prefix
     case_insensitive=True, # Commands aren't case-sensitive
     intents = intents # Set up basic permissions
 )
 
-bot.author_id = 0000000  # Change to your discord id
+bot.author_id = "ScottPosey#6036"  # Change to your discord id
 
 @bot.event
 async def on_ready():  # When the bot is ready
@@ -21,5 +24,46 @@ async def on_ready():  # When the bot is ready
 async def pong(ctx):
     await ctx.send('pong')
 
-token = "<MY_TOKEN>"
+@bot.command()
+async def name(ctx):
+    await ctx.send(ctx.author.name)
+
+@bot.command()
+async def d6(ctx):
+    var = random.randint(1, 6)
+    await ctx.send(var)
+
+@bot.event
+async def on_message(message):
+    if (message.content == "Salut tout le monde"):
+        msg = "Salut tout seul"
+        await message.channel.send(msg)
+        await message.channel.send(message.author.mention)
+    await bot.process_commands(message)
+
+@bot.command()
+async def count(ctx):
+    i = 0
+    dnd = 0
+    offline = 0
+    online = 0
+    idle = 0
+    while i < len(ctx.guild.members):
+        if (str(ctx.guild.members[i].status) == "dnd"):
+            dnd += 1
+        if (str(ctx.guild.members[i].status) == "offline"):
+            offline += 1
+        if (str(ctx.guild.members[i].status) == "online"):
+            online = online + 1
+        if (str(ctx.guild.members[i].status) == "idle"):
+            idle = idle + 1
+        i = i + 1
+
+    await ctx.send(dnd + "dnd")
+    await ctx.send(offline + "offline")
+    await ctx.send(online + "online")
+    await ctx.send(idle + "idle")
+   
+    
+token = ""
 bot.run(token)  # Starts the bot
